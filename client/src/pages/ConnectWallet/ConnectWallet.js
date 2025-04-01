010095db3b2fa082086a2059d702c26bcd915234
Fatma-Dev2025 <fatmaibrahim922003@gmail.com>
2025-04-01 19:36:28 +0200

    Update ConnectWallet.js

--- client/src/pages/ConnectWallet/ConnectWallet.js
+++ client/src/pages/ConnectWallet/ConnectWallet.js
@@ -1,42 +1,32 @@
-import React, { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useLocation } from 'react-router-dom';
-import Swal from 'sweetalert2'
-import globalContext from './../../context/global/globalContext'
-import LoadingScreen from '../../components/loading/LoadingScreen'
 
-import socketContext from '../../context/websocket/socketContext'
-import { CS_FETCH_LOBBY_INFO } from '../../pokergame/actions'
-import './ConnectWallet.scss'
+import React, { useContext, useEffect } from 'react' import { useNavigate } from 'react-router-dom' import { useLocation } from 'react-router-dom'; import Swal from 'sweetalert2' import globalContext from './../../context/global/globalContext' import LoadingScreen from '../../components/loading/LoadingScreen'
 
-const ConnectWallet = () => {
-  const { setWalletAddress, setChipsAmount } = useContext(globalContext)
-   
-  const { socket } = useContext(socketContext)
-  const navigate = useNavigate()
-  const useQuery = () => new URLSearchParams(useLocation().search);
-  let query = useQuery()
+import socketContext from '../../context/websocket/socketContext' import { CS_FETCH_LOBBY_INFO } from '../../pokergame/actions' import './ConnectWallet.scss'
 
-  useEffect(() => {
-    if(socket !== null && socket.connected === true){
-      const walletAddress = query.get('walletAddress')
-      const gameId = query.get('gameId')
-      const username = query.get('username')
-      if(walletAddress && gameId && username){
-        console.log(username)
-        setWalletAddress(walletAddress)
-        socket.emit(CS_FETCH_LOBBY_INFO, { walletAddress, socketId: socket.id, gameId, username })
-        console.log(CS_FETCH_LOBBY_INFO, { walletAddress, socketId: socket.id, gameId, username })
-        navigate('/play')
+const ConnectWallet = () => { const { setWalletAddress, setChipsAmount } = useContext(globalContext) const { socket } = useContext(socketContext) const navigate = useNavigate() const useQuery = () => new URLSearchParams(useLocation().search); let query = useQuery()
+
+useEffect(() => { // if (window.ethereum) { const provider = new ethers.providers.Web3Provider(window.ethereum); provider.send("eth_requestAccounts", []) .then((accounts) => { const walletAddress = accounts[0]; setWalletAddress(walletAddress);
+
+// 
+      if (socket !== null && socket.connected === true) {
+        const gameId = query.get('gameId');
+        const username = query.get('username');
+        if (gameId && username) {
+          socket.emit(CS_FETCH_LOBBY_INFO, { walletAddress, socketId: socket.id, gameId, username });
+          navigate('/play');
+        }
       }
-    }
-  }, [socket])
-
-  return (
-    <>
-      <LoadingScreen />
-    </>
-  )
+    })
+    .catch((err) => {
+      Swal.fire("Error", "Please install MetaMask or connect to Ethereum wallet", "error");
+    });
+} else {
+  Swal.fire("Error", "MetaMask not detected", "error");
 }
 
-export default ConnectWallet
+}, [socket]);
+
+return ( <> <LoadingScreen /> </> ) }
+
+export default ConnectWallet 
+
